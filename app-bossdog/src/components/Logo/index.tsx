703392122a39acc55723636468d8ca33e71cb5dd
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-09-11 21:01:43 -0300

    Create Logo component

---
+++ app-bossdog/src/components/Logo/index.tsx
@@ -0,0 +1,5 @@
+import logo from '../../assets/logo.png'
+
+const Logo = () => <img alt="BossDog logo" src={logo} />
+
+export default Logo
--- app-bossdog/src/main.tsx
+++ app-bossdog/src/main.tsx
@@ -3,5 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <h1>BossDog</h1>
+    <App />
 )
