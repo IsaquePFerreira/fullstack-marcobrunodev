fb1da63ab7a7f54cb6e6162464f5ca2727aaf914
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-09-12 17:09:09 -0300

    Add Action component in Header

--- app-bossdog/src/components/Header/index.tsx
+++ app-bossdog/src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import Logo from '../Logo'
 import Button from '../Button'
+import Action from '../Action'
 import './header.css'
 
 const Header = () => (
     <header>
         <Logo />
+        <Action href="#nft" content="NFT" />
+        <Action href="#token" content="token" />
+        <Action href="#tean" content="team" />
+        <Action href="#whitepaper" content="whitepaper" />
         <Button />
     </header>
 )
