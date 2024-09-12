1a9fed5c03741480ce860dfddf473395b39046fc
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-09-12 16:30:52 -0300

    Create Header component

---
+++ app-bossdog/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import Logo from '../Logo'
+import Button from '../Button'
+import './header.css'
+
+const Header = () => (
+    <header>
+        <Logo />
+        <Button />
+    </header>
+)
+
+export default Header
