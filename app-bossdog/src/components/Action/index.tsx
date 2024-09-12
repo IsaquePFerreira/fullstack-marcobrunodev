b7eea2aa1e2c21bfe9ee31ff593d3dc0fa2692b2
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-09-12 17:27:12 -0300

    Add color variation to Action

--- app-bossdog/src/components/Action/index.tsx
+++ app-bossdog/src/components/Action/index.tsx
@@ -1,7 +1,7 @@
 import './action.css'
 
-const Action = ({ href, content } : { href: string, content: string }) => (
-    <a href={href}>{content}</a>
+const Action = ({ href, content, className = '' } : { href: string, content: string, className?: string }) => (
+    <a href={href} className={className}>{content}</a>
 )
 
 export default Action
